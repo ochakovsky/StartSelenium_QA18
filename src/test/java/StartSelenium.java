7321c91b7f148a517117f942e636254370887ed6
ochakovsky <ochakovsky.a@gmail.com>
2023-02-27 23:42:51 +0200

    CW_6

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -10,24 +10,34 @@ import java.util.List;
 
 public class StartSelenium {
     WebDriver wd;
+
     @BeforeMethod
-    public void preCondition(){
+    public void preCondition() {
         System.out.println("Opening web-site");
         wd = new ChromeDriver();
 //        wd.get("https://telranedu.web.app/home");
-        wd.navigate().to("https://telranedu.web.app/home");
+//        wd.navigate().to("https://telranedu.web.app/home");
+        wd.navigate().to("https://telranedu.web.app/login");
 //        wd.navigate().forward();
 //        wd.navigate().back();
     }
     @Test
-    public void testLogin(){
+    public void testLogin() {
 //        System.out.println("Test is over");
         WebElement element = wd.findElement(By.tagName("a"));
+        WebElement form = wd.findElement(By.tagName("form"));
         List<WebElement> elements = wd.findElements(By.tagName("a"));
         System.out.println(elements.size());
+
+        WebElement root = wd.findElement(By.id("root"));
+        wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        wd.findElement(By.linkText("Registration"));
+        wd.findElement(By.name("password"));
+        wd.findElement(By.partialLinkText("LOG"));
     }
+
     @AfterMethod
-    public void postCondition(){
+    public void postCondition() {
 //        wd.close();
         wd.quit();
     }
